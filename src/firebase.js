a886dca438c3eb3c45f87e1f83ca0857a672ffde
SherazJabbar <41358432+SherazJabbar@users.noreply.github.com>
2021-12-27 19:53:55 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import { getFirestore } from "firebase/firestore";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCySXlyrBpuAQTtxLUnBUrLN3ceYbepdhQ",
-  authDomain: "vuetify-bc3dc.firebaseapp.com",
-  projectId: "vuetify-bc3dc",
-  storageBucket: "vuetify-bc3dc.appspot.com",
-  messagingSenderId: "331606506368",
-  appId: "1:331606506368:web:f71274397220dea3ba9f65",
-  measurementId: "G-G536PC9GF1",
+
 };
 
 // eslint-disable-next-line no-unused-vars
